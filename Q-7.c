64dea08469ac2cc5dd07717440db2766be43ed10
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-04-15 11:54:23 +0530

    Add files via upload

---
+++ Q-7.c
@@ -0,0 +1,27 @@
+//Q.7 Write a Program to print the below pattern using nested for loop.
+
+//	A
+//	B A
+//	C B A
+//	D C B A
+//	E D C B A
+
+
+#include<stdio.h>
+
+int main() 
+{
+    int i, j;
+    char ch;
+
+    for (i = 0, ch = 'A'; i < 5; i++, ch++) 
+	{
+        char temp = ch;
+        for (j = 0; j <= i; j++) 
+		{
+            printf("%c ", temp--);
+        }
+        printf("\n");
+    }
+
+}
